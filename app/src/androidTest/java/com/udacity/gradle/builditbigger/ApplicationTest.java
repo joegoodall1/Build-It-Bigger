d634de1423f1d00c73569e156b9287f682ff86bf
joegoodall1 <joegoodall1@gmail.com>
2015-11-05 21:13:00 +0000

    Added Async test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,42 @@
 package com.udacity.gradle.builditbigger;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.v4.util.Pair;
 import android.test.ApplicationTestCase;
+import android.text.TextUtils;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    String mJsonString = null;
+    Exception mError = null;
+    CountDownLatch signal = null;
+
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        signal = new CountDownLatch(1);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        signal.countDown();
+    }
+
+    public void testStringTask() throws InterruptedException {
+        new EndpointsAsyncTask().execute((Runnable) new Pair<Context, String>(getContext(), "Manfred"));
+        signal.await();
+        assertNull(mError);
+        assertFalse(TextUtils.isEmpty(mJsonString));
+
+
+    }
+
 }
